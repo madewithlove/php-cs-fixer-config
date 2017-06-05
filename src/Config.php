5181ceea7f8d4ffdcba3d632afb79a80054c061f
Maxime Fabre <ehtnam6@gmail.com>
2017-06-05 23:57:29 +0200

    Better support for older versions of PHP

--- src/Config.php
+++ src/Config.php
@@ -13,11 +13,13 @@ class Config extends \PhpCsFixer\Config
     {
         parent::__construct('madewithlove');
 
+        $isPhp7 = version_compare(PHP_VERSION, '7.0.0');
+
         $this
             ->setRiskyAllowed(true)
             ->setRules([
                 '@Symfony' => true,
-                'array_syntax' => ['syntax' => 'short'],
+                'array_syntax' => ['syntax' => version_compare(PHP_VERSION, '5.4.0') ? 'short' : 'long'],
                 'class_keyword_remove' => false,
                 'combine_consecutive_unsets' => true,
                 'declare_strict_types' => false,
@@ -37,6 +39,7 @@ class Config extends \PhpCsFixer\Config
                 'no_unreachable_default_argument_value' => true,
                 'no_useless_else' => true,
                 'no_useless_return' => true,
+                'list_syntax' => version_compare(PHP_VERSION, '7.1.0'),
                 'not_operator_with_space' => false,
                 'not_operator_with_successor_space' => false,
                 'ordered_class_elements' => false,
@@ -44,14 +47,14 @@ class Config extends \PhpCsFixer\Config
                 'php_unit_strict' => false,
                 'phpdoc_add_missing_param_annotation' => true,
                 'phpdoc_order' => true,
-                'pow_to_exponentiation' => true,
+                'pow_to_exponentiation' => $isPhp7,
                 'psr0' => true,
                 'random_api_migration' => false,
                 'semicolon_after_instruction' => true,
                 'simplified_null_return' => true,
                 'strict_comparison' => true,
                 'strict_param' => true,
-                'ternary_to_null_coalescing' => true,
+                'ternary_to_null_coalescing' => $isPhp7,
             ]);
     }
 
